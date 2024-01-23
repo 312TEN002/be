04bbf3a9447203791b0be8d89111e74d7fbb138f
uz2ni <uzini_@naver.com>
2024-01-23 17:53:53 +0900

    test: jasypt 암호화 생성용 테스트 코드 추가

---
+++ src/test/java/com/be/poten/JasyptConfigTest.java
@@ -0,0 +1,23 @@
+package com.be.poten;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+public class JasyptConfigTest {
+
+    @Test
+    void stringEncryptor() {
+        String value = "암호화할 값(수정 필요)";
+        System.out.println(jasyptEncoding(value));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "jasypt 암호화 비밀번호(수정 필요)";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+
+}
